230aa80b7ff87e0680bdb17981dcc23819a24581
Guilherme Queiróz <g.aqueiroz19@gmail.com>
2022-03-01 01:17:13 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const tabItems = document.querySelectorAll('.tab-item')
+const tabContentItems = document.querySelectorAll('.tab-content-item')
+const monthlyPriceText = document.getElementById('monthly-price-text')
+const logoImage = document.querySelector('.netflix-logo')
+
+window.addEventListener('load', setMonthlyPriceText())
+
+window.addEventListener('resize', setMonthlyPriceText)
+
+function setMonthlyPriceText () {
+  if (this.innerWidth <= 768) {
+   monthlyPriceText.textContent = 'Monthly price'
+   logoImage.src = '../img/netflix-logo-symbol.png'
+   return
+  } 
+
+  monthlyPriceText.textContent = 'Monthly price after free month ends on 22/03/2022'
+  logoImage.src = '../img/netflix-logo.png'
+  
+}
+
+function removeBorder () {
+  tabItems.forEach(item => {
+    item.classList.remove('tab-border')
+  })
+}
+
+function hideContent () {
+  tabContentItems.forEach(item => {
+    item.classList.remove('show')
+  })
+}
+
+tabItems.forEach(item => item.addEventListener('click', function () {
+  removeBorder();
+  hideContent();
+
+  item.classList.add('tab-border')
+  document.querySelector(`#${item.id}-content`).classList.add('show')
+}))
